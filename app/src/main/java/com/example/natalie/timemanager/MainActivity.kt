e8619b5172e2597886952e0717a7900a873c812e
nbondarenko <nataliya.bondarenko17@gmail.com>
2018-05-13 13:07:40 +0300

    Make request to trello

--- app/src/main/java/com/example/natalie/timemanager/MainActivity.kt
+++ app/src/main/java/com/example/natalie/timemanager/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.custom.async
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 import java.net.URL
 
@@ -19,12 +20,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        doAsync {
-//            val result = URL("https://api.trello.com/1/boards/LFFSZcYL/?cards=all&key=2305ecf8603d372cc910909bdfc69f53&token=8d66af343986ceaf9d0cba5e291d271fd906934c5d61d245c34c5d9a7ab7a25c").readText()
-//            uiThread {
-//                Log.d("Request", result)
-//            }
-//        }
+        doAsync {
+            val result = URL("https://api.trello.com/1/boards/LFFSZcYL/?cards=all&key=2305ecf8603d372cc910909bdfc69f53&token=8d66af343986ceaf9d0cba5e291d271fd906934c5d61d245c34c5d9a7ab7a25c").readText()
+            uiThread {
+                Log.d("Request", result)
+                toast(result)
+            }
+        }
         listTickets.add(Ticket("1", "Lab1", "CV", "aaa", "", "desc1", true))
         listTickets.add(Ticket("2", "Lab2", "CV", "aaa", "", "desc1", true))
 
@@ -34,9 +36,4 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Click on " + listTickets[position].title, Toast.LENGTH_SHORT).show()
         }
     }
-
-//    protected fun onStop() {
-//        super.onStop()
-//        requestQueue?.cancelAll(TAG)
-//    }
 }
