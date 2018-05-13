9dd2b36bdb0f854f65974b13c8290b852dd08981
nbondarenko <nataliya.bondarenko17@gmail.com>
2018-05-13 19:27:33 +0300

    Added timer activity

--- app/src/main/java/com/example/natalie/timemanager/Ticket.kt
+++ app/src/main/java/com/example/natalie/timemanager/Ticket.kt
@@ -1,5 +1,3 @@
 package com.example.natalie.timemanager
 
-import java.time.LocalDateTime
-
-class Ticket(val id: String, val title: String, val due: String, val url: String, val desc: String, val closed: Boolean, val labels: String)
+class Ticket(val id: String, val title: String, val due: String, val url: String, val desc: String, val closed: Boolean, val labels: String, val timeSpent: Int = 0 )
---
+++ app/src/main/java/com/example/natalie/timemanager/TimerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.natalie.timemanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
